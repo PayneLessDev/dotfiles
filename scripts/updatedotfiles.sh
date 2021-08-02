b34f6b965a36018f4405df970bd847adbfb92948
Adam Payne <contact@adam-payne.info>
2021-08-02 15:26:58 +1000

    added scripts folder

---
+++ scripts/updatedotfiles.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+files=(
+    ".zshrc"
+    ".Xresources"
+    ".vimrc"
+    ".tmux.conf"
+    ".signature"
+    ".muttrc"
+    ".gitconfig"
+    ".bashrc"
+    ".bash_aliases"
+    ".config/kitty/kitty.conf"
+    ".config/ranger/rc.conf"
+    ".config/starship.toml")
+
+for i in ${files[*]}; do
+    rsync -havP ~/$i ~/Projects/dotfiles
+done
+
